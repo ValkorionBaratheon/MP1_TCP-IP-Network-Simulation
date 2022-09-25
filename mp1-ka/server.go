5eafc382745730ec628fe0ee0b65d72293a644b3
Komi Alasse <alasse@bc.edu>
2022-09-25 13:12:16 -0400

    add README and server.go boilerplate code

--- mp1-ka/server.go
+++ mp1-ka/server.go
@@ -9,11 +9,12 @@ func unicast_receive(source string, message []byte) {
 func main() {
 
 	// Server should read its port from the config file.
-	ln, _:= net.Listen("tcp", 3)
+	ln, _ := net.Listen("tcp", "host:port")
 	defer ln.Close()
-
 	for {
-
+		// Can receive messages from cients here.
+		conn, _ := ln.Accept()
+		_ = conn
 	}
 
 }
